38d03db1b9d8bd7637aab94aa083da116825e75d
kd <erm.melnik@gmail.com>
2025-02-15 19:12:29 +0100

    Added getDayOfWeek() & printAllDays() methods.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        int selectedDay = 1;
+        String selectedLang = "eng";
+
+        System.out.println("--- Print Selected Day ---");
+        DayOfWeek.getDayOfWeek(selectedDay, selectedLang);
+
+        System.out.println("\n--- Print All ---");
+        printAllDays(selectedLang);
+    }
+
+    public static void printAllDays(String selectedLang) {
+        int count = 1;
+        for (int i = 0; i < DayOfWeek.values().length; i++) {
+            DayOfWeek.getDayOfWeek(count++, selectedLang);
+        }
     }
 }
