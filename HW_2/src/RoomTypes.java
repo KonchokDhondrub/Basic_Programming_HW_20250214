83228515b3d5f787ed188be14b44753ed4aaf36d
kd <erm.melnik@gmail.com>
2025-02-15 22:35:31 +0100

    Created list of rooms (getters, toString, constructor)

--- HW_2/src/RoomTypes.java
+++ HW_2/src/RoomTypes.java
@@ -1,2 +1,33 @@
 public enum RoomTypes {
+    STANDARD("Standard", 100, "Стандартный однокомнатный номер"),
+    STUDIO("Studio", 120, "Однокомнатный номер с мини-кухней"),
+    SUITE("Suite", 150, "Номер улучшенной планировки"),
+    SUITE2ROOMS("Suite 2 rooms", 180, "Номер улучшенной планировки, из двух комнат"),
+    DELUXE("Deluxe", 250, "Номер повышенного комфорта"),
+    PRESIDENT("President", 500, "Самые роскошные номера");
+
+    private final String name;
+    private final int price;
+    private final String description;
+
+    RoomTypes(String name, int price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Название: %s, Цена: %d, Описание: %s", getName(), getPrice(), getDescription());
+    }
 }
