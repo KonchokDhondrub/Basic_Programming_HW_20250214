e07ba82f0ed4de757c486a6b2aadd85bdc91a896
kd <erm.melnik@gmail.com>
2025-02-15 22:34:16 +0100

    Added printAll methods

--- HW_2/src/Main.java
+++ HW_2/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        // All Room Types
+//        printAllRoomTypes();
+
+        printAllRooms();
+    }
+
+    public static void printAllRooms(){
+        for (int i = 0; i < Hotel.LISTOFROOMS.size(); i++) {
+            System.out.println(Hotel.LISTOFROOMS.get(i).toString());
+        }
+    }
+    public static void printAllRoomTypes(){
+        RoomTypes[] roomTypes = RoomTypes.values();
+        for (RoomTypes roomType : roomTypes) {
+            System.out.println(roomType);
+        }
     }
 }
