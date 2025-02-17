82b2126ff1e6604179003dd37cbd49a562f610ff
kd <erm.melnik@gmail.com>
2025-02-17 10:06:05 +0100

    Comment added

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int selectedDay = 1;
-        int selectedLang = 1; // ENG = 1; RUS = 2;
+        int selectedLang = 1; // ENG = 1; RUS = 2; // Have to Enum class create
 
         System.out.println("--- Print Selected Day ---");
         DayOfWeek.getDayOfWeek(selectedDay, selectedLang);
